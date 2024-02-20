2d8a3d64186eff15c50cbd8b26e56ca9d44f3ed6
alexansaa <alexan.saa@gmail.com>
2024-02-20 17:41:20 -0500

    sebas changes

--- src/app/consult/consult.component.ts
+++ src/app/consult/consult.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../rooms_services/api.service';
+import { Reservation } from '../models/MyData';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-consult',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgIf
+  ],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.css'
 })
-export class ConsultComponent {
+export class ConsultComponent{
+  reservation: Reservation = {
+    checkin_date : new Date(),
+    checkout_date: new Date(),
+    qty_guests: 0,
+    rooms: [],
+    total_price: 0,
+    user_id: '0',
+    _id: '0'
+  };
+error: any;
 
+  constructor(
+    private apiService: ApiService,
+  ) { }
+
+  search() {
+    this.apiService.reservationSearch(this.reservation).subscribe({
+      next: (response: Reservation) => {
+        // Handle successful response here
+        console.log('Received data from backend:', response);
+        this.reservation = response;
+        // Access and process the response data (e.g., display in the UI)
+
+      },
+      error: (error: HttpErrorResponse) => {
+        // Handle errors gracefully
+        console.error('Error fetching data:', error);
+        // Display an error message to the user
+      }
+    });
+  }
 }
