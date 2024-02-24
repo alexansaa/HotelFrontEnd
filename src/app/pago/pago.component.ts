fc60d176b8db5e355af0f84bf995e652e8346d17
Pablin72 <pablo.arcos01@epn.edu.ec>
2024-02-23 22:32:22 -0500

    Botones PayPal integrados en página de pago

--- src/app/pago/pago.component.ts
+++ src/app/pago/pago.component.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxPayPalModule, IPayPalConfig, ICreateOrderRequest  } from 'ngx-paypal';
+
+interface OrderData {
+  orderId: string;
+  // ... Otros campos de tu orden
+}
 
 @Component({
   selector: 'app-pago',
   standalone: true,
-  imports: [ ],
+  imports: [NgxPayPalModule],
   templateUrl: './pago.component.html',
-  styleUrl: './pago.component.css'
+  styleUrls: ['./pago.component.css']
 })
-export class PagoComponent{
+export class PagoComponent implements OnInit{
+
+  public payPalConfig ? : IPayPalConfig;
+
+    ngOnInit(): void {
+        this.initConfig();
+    }
+
+    private initConfig(): void {
+        this.payPalConfig = {
+            currency: 'EUR',
+            clientId: 'sb',
+            createOrderOnClient: (data) => < ICreateOrderRequest > {
+                intent: 'CAPTURE',
+                purchase_units: [{
+                    amount: {
+                        currency_code: 'EUR',
+                        value: '9.99',
+                        breakdown: {
+                            item_total: {
+                                currency_code: 'EUR',
+                                value: '9.99'
+                            }
+                        }
+                    },
+                    items: [{
+                        name: 'Enterprise Subscription',
+                        quantity: '1',
+                        category: 'DIGITAL_GOODS',
+                        unit_amount: {
+                            currency_code: 'EUR',
+                            value: '9.99',
+                        },
+                    }]
+                }]
+            },
+            advanced: {
+                commit: 'true'
+            },
+            style: {
+                label: 'paypal',
+                layout: 'vertical'
+            },
+            onApprove: (data, actions) => {
+                console.log('onApprove - transaction was approved, but not authorized', data, actions);
+
+
+            },
+            onClientAuthorization: (data) => {
+                console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+            },
+            onCancel: (data, actions) => {
+                console.log('OnCancel', data, actions);
+
+            },
+            onError: err => {
+                console.log('OnError', err);
+            },
+            onClick: (data, actions) => {
+                console.log('onClick', data, actions);
+            }
+        };
+    }
 
 }
+
+
